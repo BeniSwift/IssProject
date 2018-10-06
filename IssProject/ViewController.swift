495cdfb6ae437a3b2831d51553d1536b0d7c59ce
Benijjane Hicham <benijjane.hicham@gmail.com>
2018-10-06 21:00:49 +0100

    Initial Commit

---
+++ IssProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IssProject
+//
+//  Created by hicham on 10/6/18.
+//  Copyright © 2018 hicham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
